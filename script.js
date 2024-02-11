447665df7b3f6599d8c89ade4402b2eeaf2fa8f6
Vadim J <136094345+shy-Vadim@users.noreply.github.com>
2024-02-12 01:04:54 +0300

    Numbers event listeners done

--- script.js
+++ script.js
@@ -13,39 +13,50 @@ numbers.forEach((number) => {
   number.addEventListener("click", () => {
     if (number1.length <= 8 && operator == undefined) {
       number1 += number.id
+      calculator.display = number1
+      updateDisplay()
+    } else if (number2.length <= 8 && operator !== undefined) {
+      number1 += number.id
+      calculator.display = number2
+      updateDisplay()
     }
   })
 })
+
 //OBJECTS
 const calculator = {
-  operation: "0",
+  display: "0",
 
   add(num1, num2) {
-    return num1 + num2
+    return +num1 + +num2
   },
-  
+
   substract(num1, num2) {
-    return num1 - num2
+    return +num1 - +num2
   },
-  
+
   multiply(num1, num2) {
-    return num1 * num2
+    return +num1 * +num2
   },
-  
+
   divide(num1, num2) {
-    return num1 / num2
+    return +num1 / +num2
   },
-   
+
   operate() {
-    number1 = operator(number1, number2)
+    number1 = String(operator(number1, number2))
     number2 = ""
     operator = undefined
-    calculator.operation = number1
+    calculator.display = number1
     updateDisplay()
   },
-}  
+}
 
 // FUNCTIONS
 function updateDisplay() {
-  display.textContent = calculator.operation
+  if (calculator.display.length <= 8) {
+    display.textContent = calculator.display
+  } else {
+    display.textContent = NaN
+  }
 }
